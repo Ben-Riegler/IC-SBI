2fe9e7085c5b974bd02a8f5cc33755577ee09ea2
Ben Riegler <b.riegler@web.de>
2025-10-08 17:54:26 +0200

    jit

--- ecdf.py
+++ ecdf.py
@@ -1,62 +1,44 @@
-import os
 import jax
-import jax.numpy as jnp
 from jax import random
-from flax import linen as nn
-from flax.training import train_state, checkpoints
-import optax
 import matplotlib.pyplot as plt
-from typing import List, Any, Tuple
 import time
-import math
+
 jax.config.update("jax_enable_x64", True)
 
 
-def sig_matrix(y, alpha):
-
-    """
-    Compute sig matrix
-
-    Args
-        y (jnp.array): (batch, 1)
-        alpha: ()
-
-    Returns
-        log_mat (jnp.array): (batch, batch)
-    """
-
-    
-    s_diff = alpha * (y - y.T)
-    log_mat = jax.nn.sigmoid(s_diff)
-
-    return log_mat
-
+@jax.jit
 def sig_ecdf_vals(y, alpha):
     """
     Compute differentiable ECDF values (not sorted!)
 
     Args
         y (jnp.array): (batch, 1)
-        alpha: ()
+        alpha (jnp.array): (1, 1)
 
     Returns
         ecdf (jnp.array): (batch, 1)
     """
 
-    log_mat = sig_matrix(y=y, alpha=alpha) # (batch, batch)
+    s_diff = alpha * (y - y.T)
+    log_mat = jax.nn.sigmoid(s_diff)
 
     ecdf = log_mat.mean(axis=-1, keepdims=True)
 
     return ecdf
 
 if __name__ == "__main__":
+    
     key = random.PRNGKey(1)
     n = 5000
     y = random.normal(key, (n,1))
 
+    t0 = time.perf_counter()
     ecdf = sig_ecdf_vals(y, alpha=10)
-    print(y)
-    print(ecdf)
+    t1 = time.perf_counter()
+    print(f"{(t1-t0):.2f}")
+
+    # print(y)
+    # print(ecdf)
 
     plt.scatter(y, ecdf, s=1)
     plt.show()
